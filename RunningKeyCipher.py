8311750e149998a8737b87c27470ddf1c12d6ef1
Rumesa <rumesa2005@gmail.com>
2026-04-13 16:39:54 +0200

    running key decryption algorithm added

--- RunningKeyCipher.py
+++ RunningKeyCipher.py
@@ -13,3 +13,20 @@ def running_key_encryption_alg(key, plaintext):
         ciphertext += chr(c_value + ord('a'))
 
     return ciphertext
+
+def running_key_decryption_alg(key, ciphertext):
+
+    plaintext = ""
+
+    ciphertext = ciphertext.lower().replace(" ", "")
+    key = key.lower().replace(" ", "")
+
+    for i in range(len(ciphertext)):
+
+        c_value = ord(ciphertext[i]) - ord('a')
+        k_value = ord(key[i]) - ord('a')
+
+        p_value = (c_value - k_value) % 26
+        plaintext += chr(p_value + ord('a'))
+
+    return plaintext
