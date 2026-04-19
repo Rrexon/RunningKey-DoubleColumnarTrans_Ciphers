a599c6a9dfab85c3e6fff77edc80adf27d149720
Rumesa <rumesa2005@gmail.com>
2026-04-19 16:31:58 +0200

    Encryption i Running Key Cipher

--- verzioni2.py
+++ verzioni2.py
@@ -38,3 +38,42 @@ def extend_key(key, length):
 
     return (key * (length // len(key) + 1))[:length]
 
+
+#
+# ENCRYPTION FUNKSIONI
+#
+def running_key_encryption_alg(key, plaintext):
+
+    ciphertext = ""
+
+    # e standardizojmë plaintext
+    plaintext = plaintext.lower()
+
+    # marrim vetëm shkronjat për llogaritje të key
+    plaintext_letters_only = plaintext.replace(" ", "").lower()
+
+    # zgjasim key-in sipas gjatësisë së shkronjave
+    key_extended = extend_key(key, len(plaintext_letters_only))
+
+    j = 0  # index për key
+
+    for ch in plaintext:
+
+        # ruajmë hapësirat pa ndryshim
+        if ch == " ":
+            ciphertext += " "
+            continue
+
+        # konvertim shkronjash në numra (a=0 ... z=25)
+        p_value = ord(ch) - ord('a')
+        k_value = ord(key_extended[j]) - ord('a')
+
+        # formula e encryption (mod 26)
+        c_value = (p_value + k_value) % 26
+
+        ciphertext += chr(c_value + ord('a'))
+
+        j += 1
+
+    return ciphertext
+
